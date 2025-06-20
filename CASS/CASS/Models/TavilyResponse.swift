d5f87fc1f18ddd34ffad0c974838dc6d31ba3cf5
aidanc667 <aidanc103@gmail.com>
2025-06-19 22:48:00 -0700

    Delete CASS directory

--- CASS/CASS/Models/TavilyResponse.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-struct TavilyResponse: Codable {
-    let answer: String?
-    let results: [SearchResult]
-} 
--- CASS/Sources/CASS/CASS.swift
+++
@@ -1,9 +0,0 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-// Export public types
-@_exported import SwiftUI
-@_exported import KeychainAccess
-
-// This file is intentionally left empty as we're using @_exported
-// to make our dependencies available to importers of our module
